8ce5b0debc10a2740d185f65ae3190b6630cc4bf
Kevin Mulvey <kmulvey@linux.com>
2023-07-22 18:03:15 -0600

    combine diffs

--- gomod.go
+++ gomod.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
 	"os/exec"
+	"strings"
 )
 
 func backupOriginalGoMod() error {
@@ -21,5 +24,30 @@ func backupOriginalGoMod() error {
 		return err
 	}
 
+	_, err = exec.Command("/bin/bash", "-c", "git diff go.mod > diff").Output()
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func buildPatchedGoModFile() {
+	input, err := ioutil.ReadFile("myfile")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	lines := strings.Split(string(input), "\n")
+
+	for i, line := range lines {
+		if strings.Contains(line, "]") {
+			lines[i] = "LOL"
+		}
+	}
+	output := strings.Join(lines, "\n")
+	err = ioutil.WriteFile("myfile", []byte(output), 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
