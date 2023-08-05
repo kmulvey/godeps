7a41bfbcd9ecc163577a3aa0601f1eb311eae22f
Kevin Mulvey <kmulvey@linux.com>
2023-08-05 10:47:58 -0600

    process env vars in main()

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"flag"
+	"os"
+	"strings"
 
 	"github.com/kmulvey/godeps"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-
 	var githubToken, owner, repo string
+
+	// use cli params first
 	flag.StringVar(&githubToken, "token", "", "your github token")
 	flag.StringVar(&owner, "owner", "", "your github username")
 	flag.StringVar(&repo, "repo", "", "repo name")
 	flag.Parse()
+
+	// env vars override cli params, because these are most likely to be used
+	if githubRepo := os.Getenv("GITHUB_REPOSITORY"); githubRepo != "" {
+		var repoArr = strings.Split(githubRepo, "/")
+		if len(repoArr) == 2 {
+			owner = repoArr[0]
+			repo = repoArr[1]
+		}
+	}
+
+	githubToken = os.Getenv("GITHUB_TOKEN")
 
 	var err = godeps.Run(owner, repo, githubToken)
 	if err != nil {
